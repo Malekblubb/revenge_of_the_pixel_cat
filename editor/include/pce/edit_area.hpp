e71726bd637a5450b18508b78e4cc2b021cccba8
Christoph Malek <malekchristoph@gmail.com>
2014-05-22 19:32:40 +0200

    done stuff on edit area

--- editor/include/pce/edit_area.hpp
+++ editor/include/pce/edit_area.hpp
@@ -7,8 +7,12 @@
 #define PCE_EDIT_AREA_HPP
 
 
+#include <QRect>
 #include <QWidget>
 
+
+namespace Ui
+{class main_window;}
 
 namespace pce
 {
@@ -16,10 +20,20 @@ namespace pce
 	{
 		Q_OBJECT
 
+		QRect m_selected_rect;
+		QRect m_source_selected_rect;
 		bool m_graphic_preview_active;
+		bool m_mouse_pressed;
+		bool m_selected;
+		
+		class graphics_manager* m_graphicsmgr;
+		Ui::main_window* m_ui;
 		
 	public:
 		edit_area(QWidget* parent = nullptr);
+		
+		void set_graphicsmgr(class graphics_manager* gmgr);
+		void set_ui(Ui::main_window* ui);
 		
 	public slots:
 		// provide an interface for other widgets
@@ -32,8 +46,13 @@ namespace pce
 		
 		// events
 		void paintEvent(QPaintEvent*) override;
+		
+		// input
 		void keyPressEvent(QKeyEvent* ev) override;
 		void keyReleaseEvent(QKeyEvent* ev) override;
+		void mousePressEvent(QMouseEvent* ev) override;
+		void mouseMoveEvent(QMouseEvent* ev) override;
+		void mouseReleaseEvent(QMouseEvent* ev) override;
 	};
 }
 
--- editor/src/pce/edit_area.cpp
+++ editor/src/pce/edit_area.cpp
@@ -3,10 +3,16 @@
 // See LICENSE for more information.
 //
 
+#include "ui_main_window.h"
 #include <pce/edit_area.hpp>
+#include <pce/graphics_manager.hpp>
 
+#include <mlk/tools/math.h>
+
+#include <QBrush>
 #include <QKeyEvent>
 #include <QPainter>
+#include <QPaintEngine>
 #include <QStyleOption>
 #include <iostream>
 
@@ -15,8 +21,18 @@ namespace pce
 {	
 	edit_area::edit_area(QWidget* parent) :
 		QWidget{parent},
-		m_graphic_preview_active{false}
+		m_graphic_preview_active{false},
+		m_mouse_pressed{false},
+		m_selected{false}
 	{this->init();}
+	
+	
+	void edit_area::set_graphicsmgr(graphics_manager* gmgr)
+	{m_graphicsmgr = gmgr;}
+	
+	void edit_area::set_ui(Ui::main_window* ui)
+	{m_ui = ui;}
+	
 	
 	void edit_area::key_pressed(QKeyEvent* ev)
 	{this->keyPressEvent(ev);}
@@ -28,6 +44,9 @@ namespace pce
 	{
 		// set focus policy
 		this->setFocusPolicy(Qt::StrongFocus);
+	
+		// set mousetracking
+		this->setMouseTracking(true);
 		
 		// set stylesheet
 		this->setStyleSheet("background-image:url(://assets/checker.png);");
@@ -40,6 +59,21 @@ namespace pce
 	    opt.init(this);
 		QPainter p{this};
 	    this->style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
+		
+		// get current selected image
+		const QImage* current_img{nullptr};
+		if(m_ui->lw_tilesets->currentIndex().row() != -1)
+			current_img = &m_graphicsmgr->images().at(m_ui->lw_tilesets->currentItem()->text().toStdString());
+		
+		// draw image preview on space key press		
+		if(m_graphic_preview_active && m_ui->lw_tilesets->currentIndex().row() != -1)
+			p.drawImage(QPoint{0, 0}, *current_img);
+		
+		// draw selected shape
+		p.setBrush({{0, 0, 0, 0}});
+		p.drawRect(m_selected_rect);
+		if(current_img != nullptr)
+			p.drawImage(m_selected_rect, *current_img, m_source_selected_rect);
 		
 		std::cout << "repaint" << std::endl;
 		
@@ -75,5 +109,46 @@ namespace pce
 			this->repaint();
 			ev->accept();
 		}
+	}
+	
+	void edit_area::mousePressEvent(QMouseEvent* ev)
+	{
+		m_mouse_pressed = true;
+		m_selected = false;
+		
+		m_selected_rect.setX(mlk::math::round_to(ev->x(), 8));
+		m_selected_rect.setY(mlk::math::round_to(ev->y(), 8));
+		m_selected_rect.setWidth(0);
+		m_selected_rect.setHeight(0);
+		
+		m_source_selected_rect.setX(m_selected_rect.x());
+		m_source_selected_rect.setY(m_selected_rect.y());
+		m_source_selected_rect.setWidth(0);
+		m_source_selected_rect.setHeight(0);
+		this->repaint();
+	}
+	
+	void edit_area::mouseMoveEvent(QMouseEvent* ev)
+	{
+		if(m_mouse_pressed)
+		{
+			m_selected_rect.setWidth(mlk::math::round_to(ev->x() - m_selected_rect.x(), 8));
+			m_selected_rect.setHeight(mlk::math::round_to(ev->y() - m_selected_rect.y(), 8));
+		}
+		else
+		{
+			if(m_selected) // why need i "-1" here ??
+				m_selected_rect.setCoords(ev->x(), ev->y(), ev->x() + m_selected_rect.width() - 1, ev->y() + m_selected_rect.height() - 1);
+		}
+		this->repaint();
+	}
+	
+	void edit_area::mouseReleaseEvent(QMouseEvent* ev)
+	{
+		m_mouse_pressed = false;
+		m_selected = true;
+		
+		m_source_selected_rect.setWidth(m_selected_rect.width());
+		m_source_selected_rect.setHeight(m_selected_rect.height());
 	}
 }
