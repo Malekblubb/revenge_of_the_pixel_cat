7c0e14ea3719658127b2dee41f58651b1796d5cb
Christoph Malek <malekchristoph@gmail.com>
2014-06-01 11:56:15 +0200

    added tile

---
+++ editor/include/pce/tile.hpp
@@ -0,0 +1,20 @@
+//
+// Copyright (c) 2014 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef PCE_TILE_HPP
+#define PCE_TILE_HPP
+
+
+namespace pce
+{
+	struct tile
+	{
+		int index;
+		int flags;
+	};
+}
+
+
+#endif // PCE_TILE_HPP
