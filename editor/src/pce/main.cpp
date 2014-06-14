b8ce5f6f83b3011a489e82f03ee64c7f808a3546
Christoph Malek <malekchristoph@gmail.com>
2014-06-14 16:55:31 +0200

    added work to startchecker

--- editor/src/pce/main.cpp
+++ editor/src/pce/main.cpp
@@ -3,9 +3,11 @@
 // See LICENSE for more information.
 //
 
+#include <pce/constants.hpp>
 #include <pce/main_window.hpp>
 #include <pce/start_checker.hpp>
 
+#include <mlk/filesystem/fs_handle.h>
 #include <mlk/log/log.h>
 #include <mlk/tools/utils.h>
 
@@ -19,6 +21,18 @@ int main(int argc, char* argv[])
 	
 	// do checks before start app
 	pce::start_checker checker;
+	checker.add_work(
+	[](bool& success, std::string& on_error)
+	{
+		mlk::fs::dir_handle graphicsdir{pce::constants::graphics_path()};
+		if(!graphicsdir.exists())
+		{
+			success = false;
+			on_error = "The graphicsdirectory does not exist.\n"
+					   "Please create it. (Path: " + pce::constants::graphics_path() + ")";
+		}
+	});
+	checker.work();
 	if(!checker.valid())
 	{
 		QMessageBox::critical(nullptr, "Can not start the program", checker.error_string().c_str());
