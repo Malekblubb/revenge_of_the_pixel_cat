9c96c20e2984802efd3fda473e6e0d7e4230888b
Christoph Malek <malekchristoph@gmail.com>
2014-07-03 19:39:36 +0200

    added startscreen class

---
+++ editor/include/pce/startscreen.hpp
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2014 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef PCE_STARTSCREEN_HPP
+#define PCE_STARTSCREEN_HPP
+
+
+namespace pce
+{
+	class startscreen
+	{
+		
+	};
+}
+
+
+#endif // PCE_STARTSCREEN_HPP
---
+++ editor/src/pce/startscreen.cpp
@@ -0,0 +1,10 @@
+//
+// Copyright (c) 2014 Christoph Malek
+// See LICENSE for more information.
+//
+
+
+namespace pce
+{
+	
+}
