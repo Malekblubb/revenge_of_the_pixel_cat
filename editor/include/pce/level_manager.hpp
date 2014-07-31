66412bbff7d08e6fde3cc29ddf72955c06369d28
Christoph Malek <malekchristoph@gmail.com>
2014-07-31 14:21:19 +0200

    testing...

--- editor/include/pce/level_manager.hpp
+++ editor/include/pce/level_manager.hpp
@@ -7,11 +7,23 @@
 #define PCE_LEVEL_MANAGER_HPP
 
 
+#include <pce/level_generator.hpp>
+
+#include <string>
+
+
+namespace Ui
+{class main_window;}
+
 namespace pce
-{
+{	
 	class level_manager
 	{
+		level_generator m_lvgen;
+		
 		class edit_area* m_editarea;
+		class graphics_manager* m_gmgr;
+		Ui::main_window* m_ui;
 		
 	public:
 		level_manager();
@@ -19,8 +31,20 @@ namespace pce
 		void set_editarea(class edit_area* ea) noexcept
 		{m_editarea = ea;}
 		
+		void set_graphicsmgr(class graphics_manager* gmgr) noexcept
+		{
+			m_gmgr = gmgr;
+			m_lvgen.set_gmgr(gmgr);
+		}
+		
+		void set_ui(Ui::main_window* ui) noexcept
+		{
+			m_ui = ui;
+			m_lvgen.set_ui(ui);
+		}
+		
 		bool load();
-		bool save();
+		bool save(const std::string& filename);
 	};
 }
 
--- editor/src/pce/level_manager.cpp
+++ editor/src/pce/level_manager.cpp
@@ -3,11 +3,29 @@
 // See LICENSE for more information.
 //
 
+#include "ui_main_window.h"
 #include <pce/level_manager.hpp>
+
+
+
 
 
 namespace pce
 {
 	level_manager::level_manager()
 	{ }
+	
+	
+	bool level_manager::load()
+	{
+		
+	}
+	
+	bool level_manager::save(const std::string& filename)
+	{
+		if(!m_lvgen.generate())
+			return false;
+		
+		return true;
+	}
 }
