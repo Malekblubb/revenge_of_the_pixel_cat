7d513324d684b007559969e1d6a5883395870c89
Christoph Malek <malekchristoph@gmail.com>
2014-08-05 18:18:23 +0200

    added updateEvents()

--- game/include/pc/core/game_window.hpp
+++ game/include/pc/core/game_window.hpp
@@ -8,6 +8,8 @@
 
 #include <pc/common.hpp>
 
+#include <mlk/signals_slots/slot.h>
+
 namespace pc {
 	
 	class GameWindow {
@@ -21,6 +23,8 @@ namespace pc {
 		TGame& mGame;
 		
 	public:
+		mlk::slot<> onStop;
+		
 		GameWindow(sf::VideoMode videoMode, const std::string& title, TGame& game)
 			: mGameUpdater{game}
 			, mRenderWindow{videoMode, title}
@@ -41,17 +45,45 @@ namespace pc {
 				if(mNeedRecreate) recreateWindow();
 				
 				mGameUpdater.start();
-				mRenderWindow.clear();
+				
+				// update
+				updateEvents();
 				mGameUpdater.runUpdate();
+				
+				// render
+				mRenderWindow.clear();
 				mGameUpdater.runRender();				
 				mRenderWindow.display();
+				
 				mGameUpdater.end();
 			}
 			
 			return 0;
 		}
 		
+		void stop() {
+			mRunning = false;
+			onStop();
+		}
+		
 	private:
+		void updateEvents() {
+			sf::Event event;
+			while(mRenderWindow.pollEvent(event)) {
+				switch(event.type) {
+					case sf::Event::EventType::Closed: stop(); break;
+					case sf::Event::EventType::KeyPressed: break;
+					case sf::Event::EventType::KeyReleased: break;
+					case sf::Event::EventType::MouseButtonPressed: break;
+					case sf::Event::EventType::MouseButtonReleased: break;
+					case sf::Event::EventType::MouseWheelMoved: break;
+					default: break;
+				}
+				
+				mGame.onEvent(event);
+			}
+		}
+		
 		void recreateWindow() {
 			mRenderWindow.create(mVideoMode, mTitle, mWindowStyles);
 			mNeedRecreate = false;
