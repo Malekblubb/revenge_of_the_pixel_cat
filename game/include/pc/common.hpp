b51f6a3cb67d81400f70c7dbf1a6a37d4891be28
Christoph Malek <malekchristoph@gmail.com>
2014-08-08 18:32:04 +0200

    added enum class MouseWheel

--- game/include/pc/common.hpp
+++ game/include/pc/common.hpp
@@ -23,6 +23,19 @@ namespace pc {
 	// typedefs
 	using TGame = Game<GameWindow, GameUpdater>;
 	using Fd = float;
+	using Key = sf::Keyboard::Key;
+	using Button = sf::Mouse::Button;
+	using Vec2f = sf::Vector2f;
+	using Vec2i = sf::Vector2i;
+	using Vec2u = sf::Vector2u;
+	using Vec3f = sf::Vector3f;
+	using Vec3i = sf::Vector3i;
+	
+	// enums
+	enum class MouseWheel : int {
+		Down,
+		Up
+	};
 }
 
 #endif // PC_COMMON_HPP 
